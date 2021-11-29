d4a970281688f68cb155b795c9e83289f6caa57b
LyashenkoValeria <leovaldez00@mail.ru>
2021-11-29 19:17:40 +0300

    Add reading input data from console for conversion

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,2 +1,14 @@
 public class Converter {
+    private Double celsius;
+    private Double fahrenheit;
+    private Double kelvin;
+    private String input;
+
+
+    public Converter(Double celsius, Double fahrenheit, Double kelvin, String input) {
+        this.celsius = celsius;
+        this.fahrenheit = fahrenheit;
+        this.kelvin = kelvin;
+        this.input = input;
+    }
 }
